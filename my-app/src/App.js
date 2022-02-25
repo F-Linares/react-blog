04ecd04090656e6091cb302c475e513af2f4e587
Fernando Linares <fernando.linares1991@gmail.com>
2022-02-24 21:10:55 -0500

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Home from "./pages/home/Home"
 import TopBar from "./components/topbar/TopBar";
 import Single from "./pages/single/Single";
@@ -5,13 +6,17 @@ import Write from "./pages/write/Write";
 import Settings from "./pages/settings/Settings"
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
+import { BrowserRouter as Router, Switch, Route, link } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <TopBar />
-      <Register />
-    </>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
